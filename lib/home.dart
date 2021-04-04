b0653515d048dbed951cc86d909dd81037630f72
architsangal <archit.sangal@iiitb.org>
2021-04-05 01:06:40 +0530

    assets added but imgur used. Projrcts added

--- lib/home.dart
+++ lib/home.dart
@@ -630,21 +630,21 @@ class _MyHomePageState extends State<MyHomePage> {
                 "https://github.com/architsangal/Flutter-Web-Code",
                 "https://github.com/architsangal/Archit_Sangal_Portfolio",
                 "https://github.com/architsangal/Archit_Sangal_Portfolio#hi-there-",
-                "https://editor.rive.app/file/web/53754",
-                "https://editor.rive.app/file/mobile/53855",
+                //"https://editor.rive.app/file/web/53754",
+                //"https://editor.rive.app/file/mobile/53855",
               ],
               [
                 "Flutter Code",
                 "Generated JavaScript Code",
-                "Rive Animation Web",
-                "Rive Animation Mobile",
+                //"Rive Animation Web",
+                //"Rive Animation Mobile",
                 "README"
               ],
               [
                 1,
                 1,
-                2,
-                2,
+                //2,
+                //2,
                 3,
               ],
               // 1 - github,
@@ -658,14 +658,14 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
               true),
           card(
-              'https://i.imgur.com/Jo5UbOK.jpg',
-              'Car Racing 90s',
+              'https://i.imgur.com/xPAOSJD.jpg',
+              'FoodFast',
               'Primary Developer',
-              "Created this car racing game using Pygame. I enjoyed this game a lot so I recreated this game. This project was one of my early projects. It has one level. Though it can be extended to multi-levels quite easily using python.",
+              "This is a flutter project for IIITB canteen. Using this app we can order food. Pay it using upi. We can also pay at the time of recieving order. Much attention is paid to it's UI/UX aspect.But it is only made for the users of iiitb. So in order to use it, you must have a iiit outlook address.",
               "",
               [
-                "https://github.com/architsangal/Flutter-Web-Code",
-                "https://github.com/architsangal/CAR_RACE-Pygame#car_race-pygame",
+                "https://github.com/architsangal/FoodFast",
+                "https://github.com/architsangal/FoodFast/blob/master/README.md",
               ],
               ["Source Code", "README"],
               [
@@ -677,9 +677,246 @@ class _MyHomePageState extends State<MyHomePage> {
               // 3 - README
               // 4 - Certification
               [
-                'python',
-                'python3',
-                'pygame',
+                'Flutter',
+                'Dart',
+                'Rive',
+                'Android',
+                'iOS',
+                'Web',
+                'API',
+                'Firebase',
+                'Adobe Illustrator'
+              ],
+              true),
+          card(
+              'https://i.imgur.com/l7diYNb.jpg',
+              'IIT Bombay FOSSEE fellowship',
+              'Contributor',
+              "Under FOSSEE, Indian Institute of Technology, Bombay followship we made Lecture notes on Linear Algebra. It was my first fellowship and was offered to me in my first year (in 2020), under Prof. Prabhu Ramachandran. We use manim for this project(Python -based animation tool). Feel free to check out the lecture notes. Links are given below -",
+              "https://math.animations.fossee.in/contents/linear-algebra",
+              [
+                "https://github.com/architsangal/FSF-mathematics-python-code-archive/tree/master/FSF-2020/linear-algebra/linear-transformations",
+                "https://math.animations.fossee.in/contents/linear-algebra/linear-transformations/linear-transformations-(linear-maps)",
+                "https://math.animations.fossee.in/contents/linear-algebra/linear-transformations/the-four-fundamental-subspaces",
+                "https://math.animations.fossee.in/contents/linear-algebra/linear-transformations/orthonormal-bases",
+                "https://math.animations.fossee.in/contents/linear-algebra/linear-transformations/gram-schmidt-orthogonalization-process",
+                "https://github.com/architsangal/FSF-mathematics-python-code-archive/tree/master/FSF-2020/linear-algebra/linear-transformations#contributer-archit-sangal",
+                "https://drive.google.com/file/d/1mwfxXs8ktzMRagwzUrCR6PH_lInEryCi/view?usp=sharing",
+              ],
+              [
+                "Source Code",
+                "Lecture Notes: Linear Transformations (Linear Maps)",
+                "Lecture Notes: The Four Fundamental Subspaces",
+                "Lecture Notes: Orthonormal Bases",
+                "Lecture Notes: Gram-Schmidt Orthogonalization Process",
+                "README",
+                "Certificate"
+              ],
+              [
+                1,
+                5,
+                5,
+                5,
+                5,
+                3,
+                4,
+              ],
+              // 1 - github,
+              // 2 - Rive Animation
+              // 3 - README
+              // 4 - Certification
+              [
+                'Manim',
+                'Python3',
+                'Python',
+              ],
+              false),
+          card(
+              'https://i.imgur.com/K0xKidW.jpg',
+              'Android Flight Booking System',
+              'Primary Developer',
+              "Entry Project for the official programming and development club of International Institute of Information, Bangalore. It is an upgraded version of C project. This was a fully functional Flight Booking System. It is be used to book, cancel, edit or update imaginary flights booking. After booking or cancelling the tickets, we will recieve a email. This is a native java code.",
+              "",
+              [
+                "https://github.com/architsangal/Android-App-Flight-Booking-System",
+                "https://github.com/architsangal/Android-App-Flight-Booking-System/blob/master/Project%20Report.pdf",
+              ],
+              ["Source Code", "README"],
+              [
+                1,
+                3,
+              ],
+              // 1 - github,
+              // 2 - Rive Animation
+              // 3 - README
+              // 4 - Certification
+              ['Android', 'Java', 'API', 'Firebase'],
+              false),
+          card(
+              'https://i.imgur.com/lgecjGa.jpg',
+              'Air Ticketing System',
+              'Product Manager & Co Developer',
+              "Project was a group project which needed to be implemented in C. It need to perform all the test of an Airline Ticketing System. For example- log-in, booking, searching, cancelling, printing the ticket. Also, from the airline's side, it should be able to add the ticket, etc.",
+              "",
+              [
+                "https://github.com/architsangal/Air-Ticketing-System",
+                "https://github.com/architsangal/Air-Ticketing-System/blob/master/README.pdf",
+              ],
+              ["Source Code", "README"],
+              [
+                1,
+                3,
+              ],
+              // 1 - github,
+              // 2 - Rive Animation
+              // 3 - README
+              // 4 - Certification
+              ['C', 'makefile', 'Adobe Illustrator'],
+              false),
+          card(
+              'https://i.imgur.com/O8DmreA.jpg',
+              'Truck On Highway Finding Their Path',
+              'Primary Developer',
+              "There are a number of stations distributed all over the map. Packages can be shipped from any station to any other station. Stations are connected to Hubs, which are the nodes of a Highway network. Using graphs, shortest path is found.",
+              "",
+              [
+                "https://github.com/architsangal/A-Game-Show------Simple-server-and-client-programs",
+                "https://github.com/architsangal/A-Game-Show------Simple-server-and-client-programs/blob/main/Project%20Report.pdf",
+              ],
+              ["Source Code", "README"],
+              [
+                1,
+                3,
+              ],
+              // 1 - github,
+              // 2 - Rive Animation
+              // 3 - README
+              // 4 - Certification
+              // 5 - YouTube
+              ['Java', 'Server Client Interaction', "Applet Java"],
+              false),
+          card(
+              'https://i.imgur.com/oTJQeAB.png',
+              'COMMAND LINE PATTERN MATCHING PROGRAM',
+              'Co Developer',
+              'Project was a group project which needed to be implemented in Python. It is a Project which deals with making of a customised "UBUNTU TERMINAL COMMAND", changing the "PATH VARIABLE" by making changes to .bashrc, involves the use of ReGex (re module of Python) and os related modules of python',
+              "",
+              [
+                "https://github.com/architsangal/COMMAND-LINE-PATTERN-MATCHING-PROGRAM#run-the-following-commands-and-you-are-ready-for-new-customized-command-on-ubuntu-",
+                "https://github.com/architsangal/COMMAND-LINE-PATTERN-MATCHING-PROGRAM#run-the-following-commands-and-you-are-ready-for-new-customized-command-on-ubuntu-",
+              ],
+              ["Source Code", "README"],
+              [
+                1,
+                3,
+              ],
+              // 1 - github,
+              // 2 - Rive Animation
+              // 3 - README
+              // 4 - Certification
+              ['Python', "ReGex"],
+              false),
+          card(
+              'https://i.imgur.com/lOJjVka.png',
+              'Carrom Cum Air Hockey',
+              'Co Developer',
+              "This was a project developed in a Hackathon, conducted by zense. It was built on Canvas Development. This Project deals with complex Physics Phenomenon of oblique collision. The code for Collision is a part of complex physics and a lot of projection calculation.",
+              "https://architsangal.github.io/Canvas-Competition/",
+              [
+                "https://github.com/architsangal/Canvas-Competition#carrom-cum-air-hockey-new-version",
+                "https://github.com/architsangal/Canvas-Competition#contributers",
+              ],
+              ["Source Code", "README"],
+              [
+                1,
+                3,
+              ],
+              // 1 - github,
+              // 2 - Rive Animation
+              // 3 - README
+              // 4 - Certification
+              [
+                'JavaScript',
+                'HTML',
+              ],
+              false),
+          card(
+              'https://i.imgur.com/Jx3V96V.jpg',
+              'Quiz Game Show',
+              'Primary Developer',
+              "This is a simple server and client program. There is a host who conducts the show and participants/players who provide answers.",
+              "",
+              [
+                "https://github.com/architsangal/A-Game-Show------Simple-server-and-client-programs",
+                "https://github.com/architsangal/A-Game-Show------Simple-server-and-client-programs/blob/main/Project%20Report.pdf",
+                "https://youtu.be/pFS9HEVbAIU",
+              ],
+              ["Source Code", "README", "Youtube Video Explaination"],
+              [
+                1,
+                3,
+                6,
+              ],
+              // 1 - github,
+              // 2 - Rive Animation
+              // 3 - README
+              // 4 - Certification
+              // 5 - YouTube
+              [
+                'Java',
+                'Server Client Interaction',
+              ],
+              false),
+          card(
+              'https://i.imgur.com/XpDeQpA.jpg',
+              'Modifying PLY Files',
+              'Primary Developer',
+              "Used to edit ply files. It used meshlab to check it everything is working or not",
+              "",
+              [
+                "https://github.com/architsangal/Modifying-.ply-files",
+                "https://github.com/architsangal/Modifying-.ply-files/blob/main/README.md",
+              ],
+              ["Source Code", "README"],
+              [
+                1,
+                3,
+              ],
+              // 1 - github,
+              // 2 - Rive Animation
+              // 3 - README
+              // 4 - Certification
+              // 5 - YouTube
+              [
+                'cpp',
+                'c++',
+                'ply files',
+                'meshlab',
+              ],
+              false),
+          card(
+              'https://i.imgur.com/mV9lGKL.jpg',
+              'Fun Portfolio',
+              'Primary Developer',
+              "It was a fun project. I start it to get an idea of how HTML,CSS and JAvaScript work. It was one of my initial projects so please don't jugde.",
+              "https://architsangal.github.io/My-Portfolio/",
+              [
+                "https://github.com/architsangal/My-Portfolio#my-portfolio",
+                "https://architsangal.github.io/My-Portfolio/",
+              ],
+              ["Source Code", "README"],
+              [
+                1,
+                3,
+              ],
+              // 1 - github,
+              // 2 - Rive Animation
+              // 3 - README
+              // 4 - Certification
+              [
+                'JavaScript',
+                'HTML',
+                'CSS',
               ],
               false),
           card(
@@ -796,6 +1033,38 @@ class _MyHomePageState extends State<MyHomePage> {
                 _launchURL(link[i]);
               },
             )));
+      } else if (type[i] == 5) {
+        linksList.add(Tooltip(
+            message: message[i],
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  enableFeedback: true,
+                  primary: Colors.black,
+                  shape: CircleBorder(),
+                  side: BorderSide(color: Colors.blue)),
+              child: Icon(
+                Icons.link_rounded,
+              ),
+              onPressed: () {
+                _launchURL(link[i]);
+              },
+            )));
+      } else if (type[i] == 6) {
+        linksList.add(Tooltip(
+            message: message[i],
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  enableFeedback: true,
+                  primary: Colors.black,
+                  shape: CircleBorder(),
+                  side: BorderSide(color: Colors.blue)),
+              child: Icon(
+                Icons.play_arrow_rounded,
+              ),
+              onPressed: () {
+                _launchURL(link[i]);
+              },
+            )));
       }
     }
     List<Padding> skillslist = [];
@@ -851,6 +1120,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Center(
                       child: AutoSizeText(
                         projectName + ' | ' + role,
+                        textAlign: TextAlign.center,
                         maxFontSize: 25,
                         style: GoogleFonts.getFont(
                           'Nanum Gothic',
